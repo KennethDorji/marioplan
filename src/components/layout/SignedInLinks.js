5c0a708f933e524bb37fa9cf548fe5be2447537d
Kenneth Dorji <kennethdorji@gmail.com>
2019-04-21 02:19:43 -0400

    Created a SignedInLinks component

---
+++ src/components/layout/SignedInLinks.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SignedInLinks = () => {
+  return (
+    <ul className ="right"> 
+        <li><NavLink to = '/' </li>
+    </ul>   
+  );
+};
+
+export default SignedInLinks;
