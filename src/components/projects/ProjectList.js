3fa38222342ac6a53fbbb52d0e89c003be499295
Kenneth  Dorji <kennethdorji@gmail.com>
2019-04-21 18:19:46 -0400

    imported ProjectSummary

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,35 +1,15 @@
-import React from 'react'; 
+import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 const ProjectList = () => {
-    return (
-        <div className = "project-list section"> 
+	return (
+		<div className="project-list section">
+			<ProjectSummary />
+			<ProjectSummary />
+			<ProjectSummary />
+			<ProjectSummary />
+		</div>
+	);
+};
 
-            <div className = "card z-depth-0 project-summary"> 
-                <div className = "card-content gray-text text darken-3"> 
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Net Nin</p>
-                    <p className = "grey-text">3rd September, 3am</p>
-                </div>
-            </div>
-
-            <div className = "card z-depth-0 project-summary"> 
-                <div className = "card-content gray-text text darken-3"> 
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Net Nin</p>
-                    <p className = "grey-text">3rd September, 3am</p>
-                </div>
-            </div>
-
-            <div className = "card z-depth-0 project-summary"> 
-                <div className = "card-content gray-text text darken-3"> 
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Net Nin</p>
-                    <p className = "grey-text">3rd September, 3am</p>
-                </div>
-            </div>
-
-        </div>
-    )
-}
-
-export default ProjectList; 
+export default ProjectList;
