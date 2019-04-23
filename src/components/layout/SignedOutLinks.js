8a091b9a0dc6d96164cec5ba1bf920819c932a2c
Kenneth  Dorji <kennethdorji@gmail.com>
2019-04-23 15:28:37 -0400

    Added signin & signout navbar path

--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -1,13 +1,17 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const SignedOutLinks = () => {
-  return (
-    <ul className ="right"> 
-        <li><NavLink to = "/">Sign up</NavLink> </li>
-        <li><NavLink to = "/">Log in</NavLink> </li>
-    </ul>   
-  );
+	return (
+		<ul className="right">
+			<li>
+				<NavLink to="/signup">Signup</NavLink>{' '}
+			</li>
+			<li>
+				<NavLink to="/signin">Login</NavLink>{' '}
+			</li>
+		</ul>
+	);
 };
 
 export default SignedOutLinks;
