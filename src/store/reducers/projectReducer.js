93ea0fab3eaab27acc9fe636edef67c013525dde
Kenneth  Dorji <kennethdorji@gmail.com>
2019-04-23 15:56:35 -0400

    Updated the project id to all unique id

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,8 +1,8 @@
 const initState = {
 	project: [
 		{ id: '1', title: 'help me find a peach', content: 'blah blah blah' },
-		{ id: '1', title: 'help me find a apple', content: 'blah blah blah' },
-		{ id: '1', title: 'help me find a banana', content: 'blah blah blah' }
+		{ id: '2', title: 'help me find a apple', content: 'blah blah blah' },
+		{ id: '3', title: 'help me find a banana', content: 'blah blah blah' }
 	]
 };
 
