0a6efd04a97cccbaf5c989798848df765f5459c2
Kenneth  Dorji <kennethdorji@gmail.com>
2019-04-23 15:32:43 -0400

    created projectReducer

---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+	project: [
+		{ id: '1', title: 'help me find a peach', content: 'blah blah blah' },
+		{ id: '1', title: 'help me find a apple', content: 'blah blah blah' },
+		{ id: '1', title: 'help me find a banana', content: 'blah blah blah' }
+	]
+};
+
+const projectReducer = (state = initState, action) => {
+	return state;
+};
+
+export default projectReducer;
